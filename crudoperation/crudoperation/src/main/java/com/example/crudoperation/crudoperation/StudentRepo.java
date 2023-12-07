eacf15ab0354ce559cf18759d3fe840ad295c9a0
mprasad2406 <prasad79755@gmail.com>
2023-12-07 07:29:03 +0530

    ds

--- crudoperation/crudoperation/src/main/java/com/example/crudoperation/crudoperation/StudentRepo.java
+++
@@ -1,7 +0,0 @@
-package com.example.crudoperation.crudoperation;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StudentRepo extends JpaRepository<Student, Long> {
-
-}
